61e48c8d62c63f37a40ea6ae71c0e1d9710d517a
Yash Patel <yashpatel.dev@gmail.com>
2020-11-06 21:48:30 +0530

    deployment build

---
+++ docs/assets/objects/hobbies.ts
@@ -0,0 +1,30 @@
+export const hobbies = [
+    [
+        {
+            title:"Playing Guitar",
+            image:'../../assets/images/hobbies/guitar.svg'
+        },
+        {
+            title:"Cooking",
+            image:'../../assets/images/hobbies/cooking.svg'
+        },
+        {
+            title:"Reading",
+            image:'../../assets/images/hobbies/open-book.svg'
+        }
+    ],
+    [
+        {
+            title:"Playing Football",
+            image:'../../assets/images/hobbies/football.svg'
+        },
+        {
+            title:"Listening to Music",
+            image:'../../assets/images/hobbies/headphones.svg'
+        },
+        {
+            title:"Gaming",
+            image:'../../assets/images/hobbies/console.svg'
+        }
+    ]
+]
---
+++ docs/assets/objects/navbar.ts
@@ -0,0 +1,57 @@
+export const navbarItemsLeft = [
+    {
+        title:"About Me",
+        link:"/"
+    },
+    {
+        title:"Education",
+        link:"#eduction-section"
+    },
+    {
+        title:"Experience",
+        link:"#work-experience-section"
+    },
+    {
+        title:"Skills",
+        link:"#skills-section"
+    },
+    {
+        title:"Certificates",
+        link:"#certificates-section"
+    },
+    {
+        title:"Hobbies",
+        link:"#hobbies-section"
+    },
+    {
+        title:"Projects",
+        link:"#projects-section"
+    }
+]
+
+export const navbarItemsRight = [
+    {
+        title:"",
+        link:"https://github.com/yash-patel-98",
+        image:"../assets/images/social-media/GitHub-Mark-64px.png",
+        target:"_blank"
+    },
+    {
+        title:"",
+        link:"https://www.instagram.com/yash.patel.98",
+        image:"../assets/images/social-media/iconfinder_Instagram_glyph_svg_5335781.svg",
+        target:"_blank"
+    },
+    {
+        title:"",
+        link:"https://www.linkedin.com/in/yashbpatel98/",
+        image:"../assets/images/social-media/iconfinder_BW_Linkedin_glyph_svg_5305157.svg",
+        target:"_blank"
+    },
+    {
+        title:"",
+        link:"mailto: yashpatel.dev@gmail.com",
+        image:"../assets/images/social-media/mail-24px.svg",
+        target:"_blank"
+    }
+]
