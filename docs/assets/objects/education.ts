052b67ad90e2ac8428338938f43426455836646c
Yash Patel <yashpatel.dev@gmail.com>
2021-10-05 09:06:30 +0530

    Build commite

---
+++ docs/assets/objects/education.ts
@@ -0,0 +1,13 @@
+export const educationsObject: object = [
+    {
+        imgSrc: '../assets/images/manipal-university-jaipur-logo-11-237x300.png',
+        styleImg: 'height: 200px; justify-self: center;',
+        styleH5: 'float: auto; margin-top: 10px;margin-left: 50px;',
+        collegeName: 'Manipal University Jaipur',
+        pStyle: 'margin-left: 50px;',
+        duration: '08/2016 - 05/2020',
+        courseDetails: 'B-Tech in Computer Science and Engineering',
+        cgpa: '8.95',
+    },
+];
+    
