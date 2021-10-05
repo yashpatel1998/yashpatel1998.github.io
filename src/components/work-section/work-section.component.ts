6551d1c89f9b4fa726570bd243aa917a46e707a0
Yash Patel <yashpatel.dev@gmail.com>
2021-10-05 10:48:44 +0530

    Bug Fix

--- src/components/work-section/work-section.component.ts
+++ src/components/work-section/work-section.component.ts
@@ -10,6 +10,24 @@ export class WorkSectionComponent implements OnInit {
   experiences: object;
   constructor() { 
     this.experiences = experiencesObject;
+    this.filterWorkSection(experiencesObject);
+  }
+
+  filterWorkSection(experiences){
+    // Filter the experience based on duration
+    // If current date is >= duration then show else do not show
+    experiences.filter(work => {
+      let startDuration = work.duration.split('-')[0];
+      let startMonth = parseInt(startDuration.split('/')[0])
+      let startYear = parseInt(startDuration.split('/')[1]);
+      let todaysDate = new Date();
+      console.log(`startMonth = ${startMonth}\nstartYear = ${startYear}`);
+      console.log(`todaysDate.getFullYear() = ${todaysDate.getFullYear()}\todaysDate.getMonth() = ${todaysDate.getMonth()}`);
+      if(todaysDate.getFullYear() >= startYear && todaysDate.getMonth() >= startMonth)
+        return true;
+      return false;
+    })
+    this.experiences = experiences;
   }
 
   ngOnInit(): void {
